c25e35bc880117224ea91dc0876e0e299e31d97c
Adrian Palma <adrianpalmasantana@gmail.com>
2025-08-03 01:45:54 -0500

    Creation of the express app instance, use of bodyParser and the , load of the env and the MongoDB connection string (URL) in the env file. Connect to the database with mongoose.

---
+++ server/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+
+const app = express();
+app.use(bodyParser.json());
+dotenv.config();
+
+const PORT = process.env.PORT || 7000;
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URL).then(()=>{
+    console.log("Base de datos conectada correctamente.")
+    app.listen(PORT, ()=>{
+        console.log(`Servidor corriendo en el puerto ${PORT}`)
+    });
+}).catch((error)=> console.log(error));
