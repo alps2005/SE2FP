46275b635c5dfffa489cf5ccafb402b58f618677
Adrian Palma <adrianpalmasantana@gmail.com>
2025-08-04 18:10:13 -0500

    Read: code for getting the ALL data from the database using GET method which validates if the user exists or of the requests is empty.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -2,18 +2,32 @@ import Users from "../models/userModel.js";
 
 export const create = async (req, res) => {
     try {
+        
         const newUser = new Users(req.body);
         const {email} = req.body;
 
-        const userExists = await Users.findOne({email});
-        if(userExists){
+        const usrExists = await Users.findOne({email});
+        if(usrExists){
             return res.status(400).json({ message: "El usuario ya existe." })
         }
 
-        const savedData = await newUser.save();
-        res.status(200).json(savedData);
+        const svdData = await newUser.save();
+        res.status(200).json(svdData);
 
-    } catch (error) {
+    } catch (err) {
+        res.status(500).json({ errorMessage: err.message });
+    }
+}
+
+export const getAllUsers = async(req, res) => {
+    try {
+        const usrData = await Users.find();
+        if(!usrData || usrData.length === 0){
+            return res.status(404).json({message: "User data not found."});
+        }
+        res.status(200).json(usrData);
+
+    } catch (error){
         res.status(500).json({ errorMessage: error.message });
     }
 }
