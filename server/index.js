4a8625b504c2a098330c1ad59c000874cd69b7a7
Adrian Palma <adrianpalmasantana@gmail.com>
2025-08-04 16:53:30 -0500

    route imported to the app's entry point to be used.

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import route from "./routes/userRoute.js";
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,6 +14,8 @@ const MONGO_URL = process.env.MONGO_URL;
 mongoose.connect(MONGO_URL).then(()=>{
     console.log("Base de datos conectada correctamente.")
     app.listen(PORT, ()=>{
-        console.log(`Servidor corriendo en el puerto ${PORT}`)
+        console.log(`Servidor corriendo en el puerto ${PORT}`);
     });
 }).catch((error)=> console.log(error));
+
+app.use("/api", route);
