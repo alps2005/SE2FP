9377167aa4e16f9f1d04c3aa0bb831e7b112f46f
Adrian Palma <adrianpalmasantana@gmail.com>
2025-08-05 22:58:39 -0500

    Complete CRUD controller for the api on the "users" collection.

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,33 +1,71 @@
-import Users from "../models/userModel.js";
+import User from "../models/userModel.js";
 
-export const create = async (req, res) => {
+export const createUsr = async (req, res) => {
     try {
-        
-        const newUser = new Users(req.body);
+        const newUser = new User(req.body);
         const {email} = req.body;
-
-        const usrExists = await Users.findOne({email});
+        const usrExists = await User.findOne({email});
         if(usrExists){
             return res.status(400).json({ message: "El usuario ya existe." })
         }
-
         const svdData = await newUser.save();
         res.status(200).json(svdData);
-
     } catch (err) {
         res.status(500).json({ errorMessage: err.message });
     }
 }
 
-export const getAllUsers = async(req, res) => {
+export const getAllUsrs = async(req, res) => {
     try {
-        const usrData = await Users.find();
+        const usrData = await User.find();
         if(!usrData || usrData.length === 0){
-            return res.status(404).json({message: "User data not found."});
+            return res.status(404).json({message: "No existen datos de usuarios."});
         }
         res.status(200).json(usrData);
-
     } catch (error){
+        res.status(500).json({ errorMessage: error.message });
+    }
+}
+
+export const getUsrById = async(req, res) => {
+    try {
+        const idReq = req.params.id;
+        const usrFound = await User.findById(idReq);
+        if (!usrFound) {
+            return res.status(404).json({message: "Usuario no existente."});
+        }
+        res.status(200).json(usrFound);
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+}
+
+export const updateUsr = async(req, res) => {
+    try {
+        const idReq = req.params.id;
+        const usrFound = await User.findById(idReq);
+        if (!usrFound) {
+            return res.status(404).json({message: "Usuario no existente."});
+        }
+        const updatedUsr = await User.findByIdAndUpdate(idReq, req.body, {
+            new:true
+        })
+        res.status(200).json(updatedUsr);
+    } catch (error) {
+        res.status(500).json({ errorMessage: error.message });
+    }
+}
+
+export const deleteUsr = async(req, res) => {
+    try {
+        const idReq = req.params.id;
+        const usrFound = await User.findById(idReq);
+        if (!usrFound) {
+            return res.status(404).json({message: "Usuario no existente."});
+        }
+        await User.findByIdAndDelete(idReq);
+        res.status(200).json({ message: "Usuario elminado exitosamente."})
+    } catch (error) {
         res.status(500).json({ errorMessage: error.message });
     }
 }
