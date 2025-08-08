0c54ad84b0957b332f582d39137431723b488f49
Adrian Palma <adrianpalmasantana@gmail.com>
2025-08-07 19:56:31 -0500

    adduser code

--- client/src/addUser/AddUser.jsx
+++ client/src/addUser/AddUser.jsx
@@ -1,17 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
 import "./adduser.css";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const AddUser = () => {
+  const users = {
+    name: "",
+    email: "",
+    address: "",
+  };
+
+  const [user, setUser] = useState(users);
+  const navigate = useNavigate();
+
+  const inputHandler = (e) => {
+    
+    const {name, value} = e.target;
+    console.log(name, value);
+    setUser({ ...user, [name]: value });
+
+  }
+
+  const submitForm = async(e) => {
+
+    e.preventDefault();
+    await axios.post("http://localhost:8000/api/createUsr", user).then((response)=>{toast.success(response.data.message, { position: "top-right" })
+
+    navigate("/");
+
+  }).catch((error)=>{
+      console.log(error);
+    })
+  }
+
     return (
         <div className="addUser">
           <h3>Add New User</h3>
-          <form className="addUserForm" >
+          <form className="addUserForm" onSubmit={submitForm}>
             <div className="inputGroup">
               <label htmlFor="name">Nombre:</label>
               <input
                 type="text"
                 id="name"
+                onChange={inputHandler}
                 name="name"
                 autoComplete="off"
                 placeholder="Enter your Name"
@@ -22,6 +54,7 @@ const AddUser = () => {
               <input
                 type="email"
                 id="email"
+                onChange={inputHandler}
                 name="email"
                 autoComplete="off"
                 placeholder="Enter your Email"
@@ -32,6 +65,7 @@ const AddUser = () => {
               <input
                 type="text"
                 id="address"
+                onChange={inputHandler}
                 name="address"
                 autoComplete="off"
                 placeholder="Enter your Address"
